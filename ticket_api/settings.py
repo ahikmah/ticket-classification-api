36bb616f5755acb0cff11337d2e6eb401f72e859
Awaliyatul Hikmah <imadlz15@gmail.com>
2022-09-18 19:22:51 +0700

    deploy setup

--- ticket_api/settings.py
+++ ticket_api/settings.py
@@ -22,7 +22,7 @@ MODELS = os.path.join(BASE_DIR, 'ticket_classification\model')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m301(hbsc0pge+dab4wjtohu3pwrov3ns-+=(xsbjiv9uwaet4'
+SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
