cf2e63059554995ed16e24c0e428539741a1bf3f
Awaliyatul Hikmah <imadlz15@gmail.com>
2022-09-24 23:40:08 +0700

    Update settings.py

--- ticket_api/settings.py
+++ ticket_api/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 import os
-import sys
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
